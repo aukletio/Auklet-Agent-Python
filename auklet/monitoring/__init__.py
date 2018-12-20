6956822971ddb74f25f39b8c70499aa8e2ba6d0b
Tyler Wiersing <tyler.wiersing@gmail.com>
2018-12-19 23:43:02 -0500

    update tests and refactor processing file

--- auklet/monitoring/__init__.py
+++ auklet/monitoring/__init__.py
@@ -13,6 +13,7 @@ from auklet.broker import MQTTClient
 from auklet.utils import get_mac, setup_thread_excepthook, create_dir
 from auklet.monitoring.logging import AukletLogging
 from auklet.monitoring.processing import Client
+from auklet.monitoring.utils import update_data_limits
 from auklet.stats import MonitoringTree
 from auklet.errors import AukletConfigurationError
 
@@ -60,7 +61,7 @@ class Monitoring(AukletLogging):
         self.mac_hash = get_mac()
         self.client = Client(api_key, app_id, release, base_url,
                              self.mac_hash, self.version, self.auklet_dir)
-        self.emission_rate = self.client.update_limits()
+        self.emission_rate = update_data_limits(self.client)
         self.tree = MonitoringTree(self.mac_hash)
         self.broker = MQTTClient(self.client)
         self.monitor = monitoring
@@ -107,7 +108,7 @@ class Monitoring(AukletLogging):
             self.tree.clear_root()
             self.samples_taken = 0
         if self.total_samples % self.hour == 0:
-            self.emission_rate = self.client.update_limits()
+            self.emission_rate = update_data_limits(self.client)
             self.client.check_date()
 
     def handle_exc(self, type, value, traceback):
